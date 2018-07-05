1e0dc332b31661807323051ab140ec3fe2104fd7
agaesnk <agaesnk@gmail.com>
2018-07-05 10:51:55 +0200

    done

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ var ul = document.querySelector('#items-list');
 
 var cards = ['1_spade.png','2_heart.png','4_spade.png','6_diamond.png','7_club.png','queen_heart.png','king_diamond.png','8_spade.png','3_diamond.png','5_club.png','10_club.png','jack_spade.png','9_club.png'];
 
+//Añadir cartas
 var handleClick = function(){
   var li = document.createElement("li");
   li.style.backgroundImage = "url('images/carta2.png')";
@@ -11,12 +12,13 @@ var handleClick = function(){
   li.style.backgroundSize = "cover";
   ul.style.display = "flex";
   ul.style.justifyContent = "space-between";
-  //cards.push(li);
+  
   ul.appendChild(li);
 }
 
 button.addEventListener('click', handleClick);
 
+//Dar la vuelta a la carta
 ul.addEventListener('click', function(event) {
   var clickedCard = event.target;
   var random = Math.floor(Math.random() * cards.length);
